52170d6282682ea995fac9d9e1919f8b2829ae12
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2021-05-13 09:13:24 -0300

    Classes statics + Principio de Responsabilidade Única (Não finalizado)

---
+++ CadastroAnuncio/RegraDeNegocio/QtdViewDia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroAnuncio.RegraDeNegocio
+{
+    public static class QtdViewDia
+    {
+        public static double GetQtdViewDia(double investimentoDia)
+        {
+            return investimentoDia * 30;
+        }
+    }
+}
