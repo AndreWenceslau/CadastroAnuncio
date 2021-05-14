efa07e2d40a7d7a700d62293a4c48645229a42c4
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2021-05-13 21:10:32 -0300

    Criado Unit Test

---
+++ CadastroAnuncioTeste/CadastroAnuncio.cs
@@ -0,0 +1,109 @@
+﻿using CadastroAnuncio.Models;
+using CadastroAnuncio.RegraDeNegocio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CadastroAnuncioTeste
+{
+    [TestClass]
+    public class CadastroAnuncio
+    {
+        [TestMethod]
+        public void CadastraAnuncio_QuandoReceberTodosValores_DeveRetornarValorTotalInvestido()
+        {
+            //Arrange
+            CadastroAnuncioModel cadastroAnuncioModel = new CadastroAnuncioModel();
+            cadastroAnuncioModel.NomeAnuncio = "Nome Anuncio";
+            cadastroAnuncioModel.NomeCliente = "Nome Cliente";
+            cadastroAnuncioModel.DataInicio = DateTime.Now;
+            cadastroAnuncioModel.DataTermino = DateTime.Now.AddDays(1);
+            cadastroAnuncioModel.InvestimentoDia = 10;
+
+            var resultadoEsperado = 10;
+
+            //Act
+            var investimentoDia = cadastroAnuncioModel.InvestimentoDia;
+            var tempoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            cadastroAnuncioModel.ValorTotalInvestido = investimentoDia * tempoAnuncio;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, cadastroAnuncioModel.ValorTotalInvestido);
+        }
+        [TestMethod]
+        public void CadastraAnuncio_QuandoReceberTodosValores_DeveRetornarQuantidadeMaximaVisualizacoes()
+        {
+            //Arrange
+            CadastroAnuncioModel cadastroAnuncioModel = new CadastroAnuncioModel();
+            cadastroAnuncioModel.NomeAnuncio = "Nome Anuncio";
+            cadastroAnuncioModel.NomeCliente = "Nome Cliente";
+            cadastroAnuncioModel.DataInicio = DateTime.Now;
+            cadastroAnuncioModel.DataTermino = DateTime.Now.AddDays(1);
+            cadastroAnuncioModel.InvestimentoDia = 10;
+
+            var resultadoEsperado = 460;
+
+            //Act
+            var qtdViewDia = (int)QtdViewDia.GetQtdViewDia(cadastroAnuncioModel.InvestimentoDia);
+            var tempoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            var tempoDoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            cadastroAnuncioModel.QtdMaxClique = qtdViewDia * tempoAnuncio / (int)VariaveiFixas.ViewClique;
+            var compartilhamentos = TotalCompartilhamento.GetCompartilhamentos(cadastroAnuncioModel.QtdMaxClique, VariaveiFixas.CliqueCompartilhamento);
+            var viewAdicionais = ViewAdicional.GetViewAdicionais(compartilhamentos);
+            cadastroAnuncioModel.QtdMaxVizualizacao = qtdViewDia * tempoDoAnuncio + viewAdicionais;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, cadastroAnuncioModel.QtdMaxVizualizacao);
+        }
+        [TestMethod]
+        public void CadastraAnuncio_QuandoReceberTodosValores_DeveRetornarQuantidadeMaximaCliques()
+        {
+            //Arrange
+            CadastroAnuncioModel cadastroAnuncioModel = new CadastroAnuncioModel();
+            cadastroAnuncioModel.NomeAnuncio = "Nome Anuncio";
+            cadastroAnuncioModel.NomeCliente = "Nome Cliente";
+            cadastroAnuncioModel.DataInicio = DateTime.Now;
+            cadastroAnuncioModel.DataTermino = DateTime.Now.AddDays(1);
+            cadastroAnuncioModel.InvestimentoDia = 10;
+
+            var resultadoEsperado = 37;
+
+            //Act
+            var qtdViewDia = (int)QtdViewDia.GetQtdViewDia(cadastroAnuncioModel.InvestimentoDia);
+            var tempoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            var tempoDoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            cadastroAnuncioModel.QtdMaxClique = qtdViewDia * tempoAnuncio / (int)VariaveiFixas.ViewClique;
+            var compartilhamentos = TotalCompartilhamento.GetCompartilhamentos(cadastroAnuncioModel.QtdMaxClique, VariaveiFixas.CliqueCompartilhamento);
+            var viewAdicionais = ViewAdicional.GetViewAdicionais(compartilhamentos);
+            cadastroAnuncioModel.QtdMaxVizualizacao = qtdViewDia * tempoDoAnuncio + viewAdicionais;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, cadastroAnuncioModel.QtdMaxClique);
+        }
+        [TestMethod]
+        public void CadastraAnuncio_QuandoReceberTodosValores_DeveRetornarQuantidadeMaximaCompartilhamentos()
+        {
+            //Arrange
+            CadastroAnuncioModel cadastroAnuncioModel = new CadastroAnuncioModel();
+            cadastroAnuncioModel.NomeAnuncio = "Nome Anuncio";
+            cadastroAnuncioModel.NomeCliente = "Nome Cliente";
+            cadastroAnuncioModel.DataInicio = DateTime.Now;
+            cadastroAnuncioModel.DataTermino = DateTime.Now.AddDays(1);
+            cadastroAnuncioModel.InvestimentoDia = 10;
+
+            var resultadoEsperado = 4;
+
+            //Act
+            var qtdViewDia = (int)QtdViewDia.GetQtdViewDia(cadastroAnuncioModel.InvestimentoDia);
+            var tempoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            var tempoDoAnuncio = TempoDoAnuncio.GetTempoDoAnuncio(cadastroAnuncioModel.DataInicio, cadastroAnuncioModel.DataTermino);
+            cadastroAnuncioModel.QtdMaxClique = qtdViewDia * tempoAnuncio / (int)VariaveiFixas.ViewClique;
+            var compartilhamentos = TotalCompartilhamento.GetCompartilhamentos(cadastroAnuncioModel.QtdMaxClique, VariaveiFixas.CliqueCompartilhamento);
+            var viewAdicionais = ViewAdicional.GetViewAdicionais(compartilhamentos);
+            cadastroAnuncioModel.QtdMaxVizualizacao = qtdViewDia * tempoDoAnuncio + viewAdicionais;
+            cadastroAnuncioModel.QtdMaxCompartilhamento = compartilhamentos;
+
+            //Assert
+            Assert.AreEqual(resultadoEsperado, cadastroAnuncioModel.QtdMaxCompartilhamento);
+        }
+    }
+}
---
+++ CadastroAnuncioTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CadastroAnuncioTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CadastroAnuncioTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5934cfc-da4b-413b-b3e4-f4c8c593aa3c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
