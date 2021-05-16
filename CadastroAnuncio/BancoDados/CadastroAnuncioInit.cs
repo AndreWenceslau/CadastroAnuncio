15597c6021bf4bb87a7fd5af37676247fbb9d7d8
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2021-05-15 21:00:30 -0300

    Resolvido impedimento relacionado a classe CadastroAnuncioInit

--- CadastroAnuncio/BancoDados/CadastroAnuncioInit.cs
+++ CadastroAnuncio/BancoDados/CadastroAnuncioInit.cs
@@ -7,9 +7,9 @@ using System.Web;
 
 namespace CadastroAnuncio.BancoDados
 {
-    public class CadastroAnuncioInit : CreateDatabaseIfNotExists<CadastroAnuncioContext>
+    public class CadastroAnuncioInit : CreateDatabaseIfNotExists<CadastroAnuncioContexto>
     {
-        protected override void Seed(CadastroAnuncioContext context)
+        protected override void Seed(CadastroAnuncioContexto context)
         {
             List<CadastroAnuncioModel> cadastroAnuncios = new List<CadastroAnuncioModel>()
             {
@@ -25,7 +25,34 @@ namespace CadastroAnuncio.BancoDados
                     QtdMaxClique = 360,
                     QtdMaxCompartilhamento = 54
 
-                } 
+                } ,
+
+                new CadastroAnuncioModel()
+                {
+                    NomeAnuncio = "Teste Anuncio 02",
+                    NomeCliente = "Teste Cliente 02",
+                    DataInicio = DateTime.Now,
+                    DataTermino = DateTime.Now.AddDays(10),
+                    InvestimentoDia = 10,
+                    ValorTotalInvestido = 100,
+                    QtdMaxVizualizacao = 3160,
+                    QtdMaxClique = 360,
+                    QtdMaxCompartilhamento = 54
+
+                },
+                 new CadastroAnuncioModel()
+                {
+                    NomeAnuncio = "Teste Anuncio 03",
+                    NomeCliente = "Teste Cliente 03",
+                    DataInicio = DateTime.Now,
+                    DataTermino = DateTime.Now.AddDays(10),
+                    InvestimentoDia = 10,
+                    ValorTotalInvestido = 100,
+                    QtdMaxVizualizacao = 3160,
+                    QtdMaxClique = 360,
+                    QtdMaxCompartilhamento = 54
+
+                }
             };
 
             cadastroAnuncios.ForEach(c => context.CadastroAnuncio.Add(c));
--- CadastroAnuncio/Migrations/Configuration.cs
+++ CadastroAnuncio/Migrations/Configuration.cs
@@ -5,14 +5,14 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CadastroAnuncio.BancoDados.CadastroAnuncioContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CadastroAnuncio.BancoDados.CadastroAnuncioContexto>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(CadastroAnuncio.BancoDados.CadastroAnuncioContext context)
+        protected override void Seed(CadastroAnuncio.BancoDados.CadastroAnuncioContexto context)
         {
             //  This method will be called after migrating to the latest version.
 
